cf15d814f4b1b29d6769dde139e494797ac5fa73
Finn Schmidt <finn.schmidt@brainworxx.de>
2025-09-05 11:16:04 +0200

    Erstellung des Raster Simplyfiziert, Überarbeited

--- javascript.js
+++ javascript.js
@@ -6,12 +6,11 @@ document.addEventListener("DOMContentLoaded", function(){
     
 
     for (let y = 0; y < aktionsFlaecheBreite_X; y++){
-        const aktionsFlaecheSpalten_y = [];
-        const aktionsFlaecheZeilen_x = [];
-        aktionsFlaecheSpalten_y.push(aktionsFlaecheZeilen_x);
-        const neueZeile = document.createElement("div");
-        aktionsFlaeche.appendChild(neueZeile);
-        
+
+  
+        const spaltenContainer = document.createElement("div"); //Container für das Y-Array, damit es an die Aktionsfläche im Dom angehängt werden kann
+        aktionsFlaeche.appendChild(spaltenContainer); //Der Spaltencontainer für die Y-Arrays wird an die Aktionsfläche im Dom angehängt
+
         for (let x = 0; x < aktionsFlaecheBreite_X; x++){
             const zelle = document.createElement("div");
             zelle.classList.add("toteZelle");
@@ -19,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function(){
             zellGroesse = (aktionsFlaecheBreiteInPixel / aktionsFlaecheBreite_X);
             zelle.style.width = zellGroesse + "px";
             zelle.style.height = zellGroesse + "px";
-            neueZeile.appendChild(zelle);
+            spaltenContainer.appendChild(zelle);
         };
     }
 });
