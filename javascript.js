e8ddbe7dac414b4b9291cd3368a081f143154275
Finn Schmidt <finn.schmidt@brainworxx.de>
2025-09-10 13:58:02 +0200

    Grundstrucktur der Simulation funktioniert

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
-const aktionsFlaecheBreite_X = 5;
+///////////////////////////////////////////////////////////////////////////Variablen
 const aktionsFlaeche = document.getElementById("aktionsFlaeche");
+const startButton = document.getElementById("startButton");
+const übernehmenButton = document.getElementById("übernehmenButton");
+let arrayFürNeueAktionsFlaeche = [];
+const aktionsFlaecheBreite_X = 10;
+
 
 ///////////////////////////////////////////////////////////////////////////Funktion um das Raster zu generrieren
 document.addEventListener("DOMContentLoaded", ()=> {   
@@ -20,7 +25,6 @@ document.addEventListener("DOMContentLoaded", ()=> {
             spaltenContainer.appendChild(zelle);
         };
     };
-    laeuferFormation();
 });
 
 
@@ -44,55 +48,78 @@ document.addEventListener("click", function(event){
 
 
 ///////////////////////////////////////////////////////////////////////////Funktion um Aktionsfläche zu Prüfen
-const startButton = document.getElementById("startButton");
-let lebendigeZellenAngrenzend = 0;
-let arrayFürNeueAktionsFlaeche = [];
-
-
 startButton.addEventListener("click", ()=> {
-    
-    let X_KoordinateZuPruefendeZelle = 0;
-    while (X_KoordinateZuPruefendeZelle != aktionsFlaecheBreite_X){
-        let Y_KoordinateZuPruefendeZelle = 0;
-        while (Y_KoordinateZuPruefendeZelle != aktionsFlaecheBreite_X){
-            prüfenVonZelen(X_KoordinateZuPruefendeZelle, Y_KoordinateZuPruefendeZelle);
-                if (lebendigeZellenAngrenzend == 2 || lebendigeZellenAngrenzend == 3){
-                    arrayFürNeueAktionsFlaeche.push(1);
-                } else {
-                    arrayFürNeueAktionsFlaeche.push(0);
-                };
-            Y_KoordinateZuPruefendeZelle++
-        };
-        X_KoordinateZuPruefendeZelle++        
-        arrayFürNeueAktionsFlaeche.push("x");
+    arrayFürNeueAktionsFlaeche = [];
+
+    for (let x = 0; x != aktionsFlaecheBreite_X; x++){
+       
+        for (let y = 0; y != aktionsFlaecheBreite_X; y++){
+            let lebendigeZellenAngrenzend = 0;
+            angrenzendeZellenPruefen(x-1, y-1);
+            angrenzendeZellenPruefen(x, y-1);
+            angrenzendeZellenPruefen(x+1, y-1);
+            angrenzendeZellenPruefen(x+1, y);
+            angrenzendeZellenPruefen(x+1, y+1);
+            angrenzendeZellenPruefen(x, y+1);
+            angrenzendeZellenPruefen(x-1, y+1);
+            angrenzendeZellenPruefen(x-1, y);
+
+            function angrenzendeZellenPruefen(x, y){
+                const anvisierteZelle = document.getElementById(`(${x}/${y})`);
+                if (anvisierteZelle && anvisierteZelle.classList.contains("zelleLebendig")) {
+                    lebendigeZellenAngrenzend++
+                }
+            };
+
+            let zuPruefendeZelleLebendig = false;
+            const zuPruefendeZelle = document.getElementById(`(${x}/${y})`);
+            if (zuPruefendeZelle && zuPruefendeZelle.classList.contains("zelleLebendig")){
+                zuPruefendeZelleLebendig = true;
+            };
+
+            if ((zuPruefendeZelleLebendig && (lebendigeZellenAngrenzend === 2 || lebendigeZellenAngrenzend === 3)) || (!zuPruefendeZelleLebendig && lebendigeZellenAngrenzend === 3)){
+                arrayFürNeueAktionsFlaeche.push(1);
+            } else {
+                arrayFürNeueAktionsFlaeche.push(0);
+            }; 
+        }; 
     };
-    console.log(arrayFürNeueAktionsFlaeche);
+    arrayFürNeueAktionsFlaeche.push(2); //Ende Marker
+});
+
+
+///////////////////////////////////////////////////////////////////////////Funktion um neues Array zu übernehmen
+übernehmenButton.addEventListener("click", ()=> {
+    let x_Kordinate = 0;
+    let y_Kordinate = 0;
+
+    for (let positionInArray = 0; arrayFürNeueAktionsFlaeche[positionInArray] != 2 ; positionInArray++){
+        
+        const zuAktualisierendeZelle = document.getElementById(`(${x_Kordinate}/${y_Kordinate})`);
+        
+        if (arrayFürNeueAktionsFlaeche[positionInArray] == 1){
+            zuAktualisierendeZelle.style.backgroundColor = "yellow"; 
+            zuAktualisierendeZelle.classList.remove("zelleTod");
+            zuAktualisierendeZelle.classList.add("zelleLebendig");
+        } else if (arrayFürNeueAktionsFlaeche[positionInArray] == 0){
+            zuAktualisierendeZelle.style.backgroundColor = "grey"; 
+            zuAktualisierendeZelle.classList.remove("zelleLebendig");
+            zuAktualisierendeZelle.classList.add("zelleTod");
+        }
+
+        if (y_Kordinate != aktionsFlaecheBreite_X -1){
+            y_Kordinate++;
+        } else {
+            y_Kordinate = 0;
+            x_Kordinate++;
+        };
+    };
 });
 
 
 
 
 
-function prüfenVonZelen(x, y){
-    
-    lebendigeZellenAngrenzend = 0;
-    angrenzendeZellenPrüfen(x-1, y-1);
-    angrenzendeZellenPrüfen(x, y-1);
-    angrenzendeZellenPrüfen(x+1, y-1);
-    angrenzendeZellenPrüfen(x+1, y);
-    angrenzendeZellenPrüfen(x+1, y+1);
-    angrenzendeZellenPrüfen(x, y+1);
-    angrenzendeZellenPrüfen(x-1, y+1);
-    angrenzendeZellenPrüfen(x-1, y);
-
-    function angrenzendeZellenPrüfen(x, y){
-        const anvisierteZelle = document.getElementById(`(${x}/${y})`);
-
-        if (anvisierteZelle && anvisierteZelle.classList.contains("zelleLebendig")) {
-		    lebendigeZellenAngrenzend++
-	    }
-    };
-}
 
 
 
@@ -128,15 +155,7 @@ function prüfenVonZelen(x, y){
 
 
 
-
-
-
-
-
-
-
-
-
+/*
 ///////////////////////////////////////////////////////////////////////////Funktion um Läuferformation zu erstellen
 function pixelVorMarkieren(zelle){
     zelle.style.backgroundColor = "yellow"; 
@@ -158,3 +177,4 @@ function laeuferFormation(){
     const fünf = document.getElementById(`(${mitte-1}/${mitte+1})`);
     pixelVorMarkieren(fünf);
 }
+*/
